5330d8cc8422275ee4d17f160009334a9765abf5
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-07-22 15:23:26 +0200

    modify components: PickCity, WeatherBox

--- src/components/PickCity/PickCity.js
+++ src/components/PickCity/PickCity.js
@@ -1,20 +1,31 @@
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 import styles from './PickCity.module.scss';
-
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const PickCity = () => {
+const PickCity = ({ action }) => {
+
   const [city, setCity] = useState('');
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    action(city);
+    setCity('');
+  };
+
   return (
-    <form className={styles.pickCityForm}> 
+    <form className={styles.pickCityForm} onSubmit={handleSubmit}> 
       <label>
         <TextInput placeholder="Enter city name...." value={city} onChange={e => setCity(e.target.value)} />
       </label>
       <Button>Search</Button>
     </form>
   );
+};
+
+PickCity.propTypes = {
+  action: PropTypes.func.isRequired
 };
 
 export default PickCity;
--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,11 +1,17 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
 
-const WeatherBox = props => {
+const WeatherBox = () => {
+
+const handleCityChange = useCallback((city) => {
+   console.log('Selected city:', city);
+  }, []);
+
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange} />
       <WeatherSummary />
       <Loader />
     </section>
