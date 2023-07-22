0a708e1472b15f3ecc8e1a97bff3a8a614cfde83
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-07-22 19:17:51 +0200

    pass data from WeatherBox to WeatherSummary

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,34 +1,37 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 const WeatherBox = () => {
 
-const handleCityChange = useCallback((city) => {
-  
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ city }&appid=f521f15e88e06db9f19c45e1a6fa9441&units=metric`)
-  .then(res => res.json())
-  .then(data => {
+  const [weather, setWeather] = useState('');
 
-    const weatherData = {
-      city: data.name,
-      temp: data.main.temp,
-      icon: data.weather[0].icon,
-      description: data.weather[0].main
-    };
+  const handleCityChange = useCallback((city) => {
     
-    console.log(weatherData);
-  });
-  }, []);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ city }&appid=f521f15e88e06db9f19c45e1a6fa9441&units=metric`)
+    .then(res => res.json())
+    .then(data => {
 
-  return (
-    <section>
-      <PickCity action={handleCityChange} />
-      <WeatherSummary />
-      <Loader />
-    </section>
-  )
+      const weatherData = {
+        city: data.name,
+        temp: data.main.temp,
+        icon: data.weather[0].icon,
+        description: data.weather[0].main
+      };
+      console.log(weatherData);
+      
+      setWeather(weatherData);
+    });
+    }, []);
+
+    return (
+      <section>
+        <PickCity action={handleCityChange} />
+        <WeatherSummary {...weather}/>
+        <Loader />
+      </section>
+    )
 };
 
 export default WeatherBox;
@@ -39,3 +42,10 @@ export default WeatherBox;
 //przyp.: 1. met. fetch łączy z url;
 //2. then1 -  konwersja: jako .json;
 //3. then2 -  konwesja do JS i wyświetlenie danych w konsoli
+
+/*dane z f-cji handleCityChange  przekaz. (jako parametry) do komp. <WeatherSummary>:
+korzystam z ustawiania st. lok w <WeatherBox> (mogą być wtedy bez problemu przek. do komp.-dziecka za pom. parametrów,
+i mogą być w dowolnym miejscu modyf., czyli również w f-cji handleCityChange).
+Po odp. z API i przygot. danych w funkcji handleCityChange (weatherData), ustawiamy je jako st. lok. w <WeatherBox>. 
+Dalej przek. ten stan jako props do <WeatherSummary>, (tu wyk. w widoku) .
+*/
