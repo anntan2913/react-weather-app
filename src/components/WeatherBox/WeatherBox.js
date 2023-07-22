3bf3d81e32dff964563304a9d256eac97fa7925d
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-07-22 17:27:41 +0200

    fetch weather data (API response)

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -6,7 +6,20 @@ import { useCallback } from 'react';
 const WeatherBox = () => {
 
 const handleCityChange = useCallback((city) => {
-   console.log('Selected city:', city);
+  
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ city }&appid=f521f15e88e06db9f19c45e1a6fa9441&units=metric`)
+  .then(res => res.json())
+  .then(data => {
+
+    const weatherData = {
+      city: data.name,
+      temp: data.main.temp,
+      icon: data.weather[0].icon,
+      description: data.weather[0].main
+    };
+    
+    console.log(weatherData);
+  });
   }, []);
 
   return (
@@ -19,3 +32,10 @@ const handleCityChange = useCallback((city) => {
 };
 
 export default WeatherBox;
+
+
+
+
+//przyp.: 1. met. fetch łączy z url;
+//2. then1 -  konwersja: jako .json;
+//3. then2 -  konwesja do JS i wyświetlenie danych w konsoli
